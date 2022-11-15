45f8cf3e7ad096ebcbd6684d1ed321181a4388b0
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-15 15:21:19 -0300

    fix: add user repository to user module provider

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UserRepository } from './repositories/user.repository';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService, UserRepository],
   controllers: [UsersController],
 })
 export class UsersModule {}
