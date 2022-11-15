bbe5c7d304f205a40ca9c3a8e7ba9c81df92842c
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-15 15:20:54 -0300

    feat: config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import dbConfiguration from './config/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfiguration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/db.config.ts
@@ -0,0 +1,21 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => {
+  console.log('ENV', process.env.DB_MAIN_HOST, process.env.DB_MAIN_PORT);
+  return {
+    type: 'postgres',
+    logging: true,
+    host: process.env.DB_MAIN_HOST,
+    port: process.env.DB_MAIN_PORT,
+    username: process.env.DB_MAIN_USER,
+    password: process.env.DB_MAIN_PASSWORD,
+    database: process.env.DB_MAIN_DATABASE,
+    autoLoadEntities: true, // TODO remove in deploy
+    synchronize: true, // TODO remove in deploy
+    entities: ['dist/src/**/entities/*.entity{.ts,.js}'],
+    migrations: ['../shared/migrations/*{.ts,.js}'],
+    cli: {
+      migrationsDir: '../shared/migrations',
+    },
+  };
+});
