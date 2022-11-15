bd93c8502541b689397068a7721158278b942ece
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-15 17:44:53 -0300

    feat: update user repository method

--- src/modules/users/repositories/user.repository.ts
+++ src/modules/users/repositories/user.repository.ts
@@ -1,7 +1,8 @@
 import { DataSource, Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '../entities/user.entity';
 import { CreateUserDTO } from '../dto/create-user.dto';
+import { UpdateUserDTO } from '../dto/update-user.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -27,6 +28,21 @@ export class UserRepository extends Repository<User> {
 
   async createUser(createUserDTO: CreateUserDTO): Promise<User> {
     const user = this.create(createUserDTO);
+
+    await this.save(user);
+
+    return user;
+  }
+
+  async updateUser(id: string, updateUserDTO: UpdateUserDTO): Promise<User> {
+    const user = await this.preload({
+      id,
+      ...updateUserDTO,
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User ID ${id} not found`);
+    }
 
     await this.save(user);
 
