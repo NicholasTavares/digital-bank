0bbf5c411be924e95953e3da9592336f68c65350
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-15 17:27:43 -0300

    feat: create user dto

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
+import { MessagesHelper } from 'src/helpers/messagesHelper.helper';
+import { RegExHelper } from 'src/helpers/regex.helper';
+import { Match } from 'src/utils/MathDecorator.util';
+
+export class CreateUserDTO {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4, { message: 'Nome deve ter no mínimo 4 caracteres.' })
+  readonly username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(RegExHelper.password, { message: MessagesHelper.INVALID_PASSWORD })
+  readonly password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Match('password', { message: 'As senhas não são iguais.' })
+  readonly passwordConfirm: string;
+}
