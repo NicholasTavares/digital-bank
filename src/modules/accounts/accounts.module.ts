0fa13d191248d83374173590e0742771cd2bc099
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-16 16:16:15 -0300

    feat: account module

---
+++ src/modules/accounts/accounts.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountsService } from './accounts.service';
+import { Account } from './entities/account.entity';
+import { AccountsController } from './accounts.controller';
+import { AccountRepository } from './repositories/account.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Account])],
+  providers: [AccountsService, AccountRepository],
+  controllers: [AccountsController],
+})
+export class AccountsModule {}
