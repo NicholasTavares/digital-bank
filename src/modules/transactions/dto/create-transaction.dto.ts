39b447527b0bc70bb2b8e16baada09da06ec95dd
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-17 00:28:48 -0300

    feat: create transaction dto

---
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -0,0 +1,13 @@
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ToNumber } from 'src/utils/ToNumber';
+
+export class CreateTransactionDTO {
+  @IsNotEmpty({ message: 'Destinatário da transação é obrigatório!' })
+  @IsString()
+  readonly user_id: string;
+
+  @IsNotEmpty({ message: 'Valor da transação é obrigatório!' })
+  @Transform(({ value }) => (value ? ToNumber(value) : undefined))
+  readonly value: number;
+}
