ab2c37b47ddc4c7f60e2b930e8237a45a80619e5
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-17 01:03:33 -0300

    feat: auth login service

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { compareSync } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user) {
+    const payload = { sub: user.id, username: user.username };
+
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
+
+  async validateUser(username: string, password: string) {
+    let user: User;
+    try {
+      user = await this.userService.findUserByUsernameForAuth(username);
+    } catch (error) {
+      return null;
+    }
+
+    const isPasswordValid = compareSync(password, user.password);
+    if (!isPasswordValid) return null;
+
+    return user;
+  }
+}
