28546cf160c35f032815d4490e6f0233494de4cd
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-22 22:39:27 -0300

    refactor: password requirement 6 characters

--- src/helpers/messagesHelper.helper.ts
+++ src/helpers/messagesHelper.helper.ts
@@ -1,5 +1,5 @@
 export const MessagesHelper = {
   INVALID_PASSWORD:
-    'A senha deve conter letras 8 caracteres, letras maiúsculas, minúsculas, números e caracteres especiais',
+    'A senha deve conter letras 6 caracteres, letras maiúsculas, minúsculas, números e caracteres especiais',
   INVALID_PASSWORD_OR_EMAIL: 'E-mail e/ou senha são inválidos',
 };
