cc8443cb04d36ebd23b9d241308549735d7dbe26
NicholasTavares <nicholasbalby@hotmail.com>
2023-02-22 16:31:57 -0300

    refactor: add birth date column in create user dto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -8,7 +8,7 @@ import {
 } from 'class-validator';
 import { MessagesHelper } from 'src/helpers/messagesHelper.helper';
 import { RegExHelper } from 'src/helpers/regex.helper';
-import { Match } from 'src/utils/MathDecorator.util';
+import { Match } from 'src/utils/matchDecorator.util';
 
 export class CreateUserDTO {
   @IsNotEmpty({ message: 'Nome é obrigatório.' })
@@ -17,12 +17,17 @@ export class CreateUserDTO {
   readonly username: string;
 
   @IsNotEmpty({ message: 'Email é obrigatório.' })
-  @IsEmail({ message: 'Insira um email válido.' })
+  @IsEmail(
+    {},
+    {
+      message: 'Insira um email válido.',
+    },
+  )
   readonly email: string;
 
   @IsNotEmpty({ message: 'Data de nascimento é obrigatória.' })
   @IsISO8601(true, { message: 'Data inválida' })
-  readonly date_birth: Date;
+  readonly birth_date: Date;
 
   @IsNotEmpty({ message: 'Senha é obrigatória.' })
   @IsString()
@@ -30,7 +35,6 @@ export class CreateUserDTO {
   readonly password: string;
 
   @IsNotEmpty({ message: 'Confirme a senha.' })
-  @IsString()
   @Match('password', { message: 'As senhas não são iguais.' })
   readonly passwordConfirm: string;
 }
