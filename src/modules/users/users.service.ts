0916f805b502ef6b69369fecf66afb6a893bf1f1
NicholasTavares <nicholasbalby@hotmail.com>
2023-02-22 16:34:36 -0300

    feat: add get me service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,15 +3,22 @@ import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { UserRepository } from './repositories/user.repository';
+import { PaginationUsersDTO } from './dto/pagination-users.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async findAll(): Promise<User[]> {
-    const users = await this.userRepository.findAll();
+  async findAll({ text }: PaginationUsersDTO): Promise<User[]> {
+    const users = await this.userRepository.findAll(text);
 
     return users;
+  }
+
+  async findMe(user_id: string) {
+    const user = await this.userRepository.findMe(user_id);
+
+    return user;
   }
 
   async findOne(id: string): Promise<User> {
