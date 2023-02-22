b7122746711a6353b9918d2915fb6d5767cc9c11
NicholasTavares <nicholasbalby@hotmail.com>
2023-02-22 16:33:07 -0300

    refactor: update user dto omit password field

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,7 +1,6 @@
-import { OmitType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUserDTO } from './create-user.dto';
 
-export class UpdateUserDTO extends OmitType(CreateUserDTO, [
-  'passwordConfirm',
-  'password',
-] as const) {}
+export class UpdateUserDTO extends PartialType(
+  OmitType(CreateUserDTO, ['password', 'passwordConfirm'] as const),
+) {}
