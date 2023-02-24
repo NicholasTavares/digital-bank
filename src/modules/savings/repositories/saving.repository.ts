7211b3f6b2d2ff0c96b70be02bcf47b5911dc2f3
NicholasTavares <nicholasbalby@hotmail.com>
2023-02-23 21:47:54 -0300

    feat: add saving repository

---
+++ src/modules/savings/repositories/saving.repository.ts
@@ -0,0 +1,110 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { Saving } from '../entities/saving.entity';
+import { Account } from 'src/modules/accounts/entities/account.entity';
+
+@Injectable()
+export class SavingRepository extends Repository<Saving> {
+  constructor(private dataSource: DataSource) {
+    super(Saving, dataSource.createEntityManager());
+  }
+
+  async findSaving(account_id: string): Promise<Saving> {
+    const saving = await this.findOne({
+      where: {
+        account_id,
+      },
+      select: ['id', 'balance', 'yield'],
+    });
+
+    if (!saving) {
+      throw new NotFoundException(`Saving not found`);
+    }
+
+    return saving;
+  }
+
+  async depositValue(
+    accountToBeDebitedBalance: number,
+    accountToBeDebitedId: string,
+    value: number,
+  ): Promise<Saving> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    let deposit: Saving;
+
+    try {
+      const accountToBeDebitedNewBalance = accountToBeDebitedBalance - value;
+
+      const saving = await this.findSaving(accountToBeDebitedId);
+
+      const newSavingBalance = saving.balance + value;
+
+      const accountToBeDebited = await queryRunner.manager.preload(Account, {
+        id: accountToBeDebitedId,
+        balance: accountToBeDebitedNewBalance,
+      });
+
+      deposit = await queryRunner.manager.preload(Saving, {
+        id: saving.id,
+        balance: newSavingBalance,
+      });
+
+      await queryRunner.manager.save([accountToBeDebited, deposit]);
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      console.log('ERROR', err);
+      await queryRunner.rollbackTransaction();
+      throw new BadRequestException('Erro ao concluir deposito!');
+    } finally {
+      await queryRunner.release();
+    }
+
+    return deposit;
+  }
+
+  async withdrawValue(
+    savingToBeDebitedBalance: number,
+    savingToBeDebitedId: string,
+    accountToBeCreditedId: string,
+    accountToBeCreditedBalance: number,
+    value: number,
+  ): Promise<Account> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    let withdraw: Account;
+
+    try {
+      const savingToBeDebitedNewBalance = savingToBeDebitedBalance - value;
+
+      const savingToBeDebited = await queryRunner.manager.preload(Saving, {
+        id: savingToBeDebitedId,
+        balance: savingToBeDebitedNewBalance,
+      });
+
+      withdraw = await queryRunner.manager.preload(Account, {
+        id: accountToBeCreditedId,
+        balance: accountToBeCreditedBalance + value,
+      });
+
+      await queryRunner.manager.save([savingToBeDebited, withdraw]);
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      console.log('ERROR', err);
+      await queryRunner.rollbackTransaction();
+      throw new BadRequestException('Erro ao concluir deposito!');
+    } finally {
+      await queryRunner.release();
+    }
+
+    return withdraw;
+  }
+}
