b7bb9db0c55b4fe8eb73e68f36b42b2ae8357b39
NicholasTavares <nicholasbalby@hotmail.com>
2023-02-25 15:48:08 -0300

    feat: add token to verification mail

--- src/modules/jobs/jobs.module.ts
+++ src/modules/jobs/jobs.module.ts
@@ -2,14 +2,22 @@ import { Module } from '@nestjs/common';
 import { SendMailProducerService } from './send-mail-producer.service';
 import { SendMailConsumerService } from './send-mail-consumer.service';
 import { BullModule } from '@nestjs/bull';
+import { VerificationMailTokenRepository } from '../verification_mail_tokens/repositories/verification_mail_tokens.repository';
+import { VerificationMailTokensService } from '../verification_mail_tokens/verification_mail_tokens.service';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: 'send-mail-verification-queue',
     }),
+    JobsModule,
   ],
-  providers: [SendMailProducerService, SendMailConsumerService],
+  providers: [
+    SendMailProducerService,
+    SendMailConsumerService,
+    VerificationMailTokensService,
+    VerificationMailTokenRepository,
+  ],
   exports: [SendMailProducerService, SendMailConsumerService],
 })
 export class JobsModule {}
--- src/modules/jobs/send-mail-consumer.service.ts
+++ src/modules/jobs/send-mail-consumer.service.ts
@@ -1,24 +1,31 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Process, Processor } from '@nestjs/bull';
 import { Job } from 'bull';
+import { VerificationMailTokensService } from '../verification_mail_tokens/verification_mail_tokens.service';
 
 type JobEmailDataProps = {
+  user_id: string;
   username: string;
   email: string;
 };
 
 @Processor('send-mail-verification-queue')
 export class SendMailConsumerService {
-  constructor(private readonly mailService: MailerService) {}
+  constructor(
+    private readonly mailService: MailerService,
+    private readonly verificationTokenService: VerificationMailTokensService,
+  ) {}
   @Process('send-mail-verification-job')
   async sendMailJob(job: Job<JobEmailDataProps>) {
-    const { username, email } = job.data;
+    const { user_id, username, email } = job.data;
+
+    const token = await this.verificationTokenService.create(user_id);
 
     await this.mailService.sendMail({
       to: email,
       from: 'Suport Digital Bank',
       subject: 'Email verification',
-      text: `Hello, ${username}! Welcome to the Digital Bank. Please verify your email.`,
+      text: `Hello, ${username}! Welcome to the Digital Bank. Please verify your email by clicking on the following link: ${token.token}`,
     });
   }
 }
