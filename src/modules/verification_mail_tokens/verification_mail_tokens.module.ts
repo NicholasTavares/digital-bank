6cde9fd9251fdd4cd7a5e6ea05d16732997f177d
NicholasTavares <nicholasbalby@hotmail.com>
2023-02-25 15:47:38 -0300

    fix: add entity to verification email module

--- src/modules/verification_mail_tokens/verification_mail_tokens.module.ts
+++ src/modules/verification_mail_tokens/verification_mail_tokens.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VerificationMailTokensService } from './verification_mail_tokens.service';
+import { VerificationMailTokenRepository } from './repositories/verification_mail_tokens.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VerificationMailToken } from './entities/verification_mail_token.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([VerificationMailToken])],
   controllers: [],
-  providers: [VerificationMailTokensService],
+  providers: [VerificationMailTokensService, VerificationMailTokenRepository],
 })
 export class VerificationMailTokensModule {}
