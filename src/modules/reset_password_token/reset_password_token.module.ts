03bdb9b6e4c2df7c75f24012c59fe1410c182cc6
NicholasTavares <nicholasbalby@hotmail.com>
2023-03-02 09:33:48 -0300

    refactor: remove reset passoword token controller

--- src/modules/reset_password_token/reset_password_token.module.ts
+++ src/modules/reset_password_token/reset_password_token.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ResetPasswordTokenService } from './reset_password_token.service';
-import { ResetPasswordTokenController } from './reset_password_token.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResetPasswordToken } from './entities/reset_password_token.entity';
-import { UsersModule } from '../users/users.module';
 import { ResetPasswordTokenRepository } from './repositories/reset_password_token.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ResetPasswordToken]), UsersModule],
-  controllers: [ResetPasswordTokenController],
+  imports: [TypeOrmModule.forFeature([ResetPasswordToken])],
+  controllers: [],
   providers: [ResetPasswordTokenService, ResetPasswordTokenRepository],
   exports: [ResetPasswordTokenService, ResetPasswordTokenRepository],
 })
