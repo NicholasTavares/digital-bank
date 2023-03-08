daa2792eddae06265d5846afd09b5f489347a5dc
NicholasTavares <nicholasbalby@hotmail.com>
2023-03-08 19:42:37 -0300

    feat: config aws service

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FormatErrors } from './utils/formatErros.util';
+import { config } from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +17,13 @@ async function bootstrap() {
         new BadRequestException(FormatErrors(errors)),
     }),
   );
+  const configService = app.get(ConfigService);
+  config.update({
+    accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
+    region: configService.get('AWS_REGION'),
+  });
+
   await app.listen(5001);
 }
 bootstrap();
