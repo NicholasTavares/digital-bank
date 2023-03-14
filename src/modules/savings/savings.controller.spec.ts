a95a527f77f763c3e57d84a454c9d0290b7eca23
NicholasTavares <nicholasbalby@hotmail.com>
2023-03-14 12:26:39 -0300

    test: get my saving

--- src/modules/savings/savings.controller.spec.ts
+++ src/modules/savings/savings.controller.spec.ts
@@ -1,14 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SavingsController } from './savings.controller';
 import { SavingsService } from './savings.service';
-import { Saving } from './entities/saving.entity';
-import { UsersModule } from '../users/users.module';
 import { SavingRepository } from './repositories/saving.repository';
-import { AccountsModule } from '../accounts/accounts.module';
 import { AccountsService } from '../accounts/accounts.service';
+import { SavingSummary } from './interfaces';
 
 describe('SavingsController', () => {
   let controller: SavingsController;
+  let service: SavingsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,6 +17,7 @@ describe('SavingsController', () => {
         {
           provide: SavingsService,
           useValue: {
+            findOne: jest.fn(),
             findSaving: jest.fn(),
             depositValue: jest.fn(),
             withdrawValue: jest.fn(),
@@ -40,9 +40,35 @@ describe('SavingsController', () => {
     }).compile();
 
     controller = module.get<SavingsController>(SavingsController);
+    service = module.get<SavingsService>(SavingsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findMe', () => {
+    it('should call the service with the correct account_id', async () => {
+      const req = {
+        user: { account_id: '36c4c1e8-d8aa-4ce5-91b1-1ba0d72a310d' },
+      } as unknown as Request;
+
+      const result: SavingSummary = {
+        id: '36c4c1e8-d8aa-4ce5-91b1-1ba0d72a310d',
+        balance: 1000,
+        yield: 50,
+      };
+
+      jest
+        .spyOn(service, 'findOne')
+        .mockImplementation(() => Promise.resolve(result));
+
+      const response = await controller.findMe(req);
+
+      expect(service.findOne).toHaveBeenCalledWith(
+        '36c4c1e8-d8aa-4ce5-91b1-1ba0d72a310d',
+      );
+      expect(response).toEqual(result);
+    });
   });
 });
