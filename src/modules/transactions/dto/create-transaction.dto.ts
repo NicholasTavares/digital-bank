ddfa2ac2580cfe1edc3146311305f155e51a11a1
NicholasTavares <nicholasbalby@hotmail.com>
2023-03-26 17:13:56 -0300

    docs: transaction dto

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,12 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTransactionDTO {
   @IsNotEmpty({ message: 'Destinatário da transação é obrigatório!' })
   @IsString()
+  @ApiProperty({
+    example: '0990e075-c9ad-4554-a761-838d22bf5fcb',
+    description: 'User ID (uuid) of a existing user.',
+  })
   readonly credited_user_id: string;
 
   @IsNotEmpty({ message: 'Valor da transação é obrigatório!' })
+  @IsString()
   @Transform(({ value }) => (value ? +value : undefined))
+  @ApiProperty({
+    example: '8',
+    description: 'Money the user wants to send.',
+  })
   readonly value: number;
 }
