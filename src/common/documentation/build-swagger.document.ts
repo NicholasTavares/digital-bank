020ea1cddc175b013466e8e746c594ca861e1923
NicholasTavares <nicholasbalby@hotmail.com>
2023-03-26 17:14:36 -0300

    refactor: remove schemas section and update description

--- src/common/documentation/build-swagger.document.ts
+++ src/common/documentation/build-swagger.document.ts
@@ -3,12 +3,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 export const setupDocumentation = (app) => {
   const config = new DocumentBuilder()
     .setTitle('Digital Bank API')
-    .setDescription('Digital Bank API documentation.')
+    .setDescription('Digital Bank API endpoints documentation.')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+
+  delete document.components.schemas;
 
   SwaggerModule.setup('documentation', app, document);
 };
